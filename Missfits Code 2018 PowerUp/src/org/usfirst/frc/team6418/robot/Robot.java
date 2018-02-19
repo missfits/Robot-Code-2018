c7d5279fd04b4f98235b700c7d5c380750d4f925
Vineel Adusumilli <younggolddar@gmail.com>
2018-02-19 23:40:53 +0100

    Manual controls (#5)
    * Wrote test talon code that worked.
    
    * Clean up
    
    * Cleaned up and formatted
    
    * Wrote WPITalon class for using Talon SRX with WPI library.
    
    * connected two joysticks for mecanum, have the WPITalon class: use one
    stick for x/y, other stick for twisting
    
    * we hooked everything up to the drive train, the joysticks make the 
    
    wheels turn. next objectives: camera code in this branch, pneumatics
    
    * Robot w/ Basic Pneumatic Code
    
    Contains code that makes pneumatic piston shoot out and in with press of
    right trigger
    
    * added 4 victors for intake and climber, added spark for elevator. no
    joystick commands for subsystems yet.
    
    * just committing for funsies... idk if we've already committed these
    changes, but i'm about to start a new branch. added a limit switch but
    didn't write code for it. Imported DigitalInput class.
    
    * Instantiated buttons on xBox controller
    
    * have all the xBox buttons joysticks triggers instantiated. Have code for
    intake: if right trigger IN, left trigger OUT, no triggers 0. 
    gyro is here.
    no navx. 
    climber is set but no logic.
    
    * Removed artifacts
    
    * Added .DS_Store to .gitignore
    
    * Recovered from laptop
    
    * added new pneumatics code: kforward at first, kreverse if BACK pressed,
    kforward again if START pressed. this is for the new pneumatics climber
    arm deploy. not yet tested.
    
    * halie started some weird logic stuff that isn't done. it's in a
    different method. the things we should do is in the slack in a picture.
    WOW ELEVATOR MAY WORK SOON YAY
    
    * i worked on the state variable, don't have code yet to actually use it
    so i left the red x's
    
    * Fixed directions of things so they don't break.

--- Missfits Code 2018 PowerUp/src/org/usfirst/frc/team6418/robot/Robot.java
+++ Missfits Code 2018 PowerUp/src/org/usfirst/frc/team6418/robot/Robot.java
@@ -1,23 +1,29 @@
 //Robot-Code-2018 from Missfits github Acc
 package org.usfirst.frc.team6418.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 import org.usfirst.frc.team6418.robot.commands.ExampleCommand;
 import org.usfirst.frc.team6418.robot.subsystems.ExampleSubsystem;
 
+import java.util.*;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
-//import edu.wpi.first.wpilibj.RobotDrive;
-//import edu.wpi.first.wpilibj.RobotDrive.MotorType;
-//import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.buttons.*;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -29,33 +35,73 @@ import edu.wpi.first.wpilibj.drive.MecanumDrive;
 public class Robot extends IterativeRobot {
 
 	//these next few lines of code create the mecanum drive interface for our bot 		
-//	RobotDrive robotDrive;
 	MecanumDrive robotDrive;
+	DifferentialDrive robotDrive2;
 
 	// Channels for the wheels
-	//changed them from ints to Spark SpeedControllers to work with MecanumDrive
-	final Spark kFrontLeftChannel = new Spark (2);
-	final Spark kRearLeftChannel = new Spark (3);
-	final Spark kFrontRightChannel = new Spark (1);
-	final Spark kRearRightChannel = new Spark (0);
 	
-
+	final DigitalInput intakeLeftLimit = new DigitalInput(0);
+	final DigitalInput intakeRightLimit = new DigitalInput(1);
+	final DigitalInput elevatorGroundLimit =new DigitalInput(2);
+	final DigitalInput elevatorSwitchLimit = new DigitalInput(3);
+	final DigitalInput elevatorScaleLimit = new DigitalInput(4);
+	final DigitalInput elevatorMaxLimit = new DigitalInput(5);
+		
+	final WPITalon kFrontLeftChannel = new WPITalon (2);
+	final WPITalon kRearLeftChannel = new WPITalon (3);
+	final WPITalon kFrontRightChannel = new WPITalon (1);
+	final WPITalon kRearRightChannel = new WPITalon (4);
+	
+	VictorSP intakeRight = new VictorSP (0);
+	VictorSP intakeLeft = new VictorSP (1);
+	
+	VictorSP climber1 = new VictorSP (2);
+	VictorSP climber2 = new VictorSP (3);
+	
+	Spark elevatorMotor = new Spark(4);
+	
 	// The channel on the driver station that the joystick is connected to
-	final int kJoystickChannel = 0;
+	final int rightJoystickChannel = 0;
+	final int leftJoystickChannel = 1;
+	final int xBoxChannel = 2;
 
-	Joystick stick = new Joystick(kJoystickChannel);
+	Joystick rightStick = new Joystick(rightJoystickChannel);
+	Joystick leftStick = new Joystick(leftJoystickChannel);
+	Joystick xBox = new Joystick(xBoxChannel);
 
 	//this next line will probably need to be changed... 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
+	// DoubleSolenoid solenoid = new DoubleSolenoid(1, 2);
+	public Compressor c = new Compressor (0);
+	public DoubleSolenoid intakeSolenoid= new DoubleSolenoid(2, 3);
+	public DoubleSolenoid climberSolenoid = new DoubleSolenoid(0,1);
+	
+	public static Timer myTimer = new Timer();
+	public static Timer solenoidTimer = new Timer();
+	
+	public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
+	public int xBoxA = 1;
+	public int xBoxB = 2;
+	public int xBoxX = 3;
+	public int xBoxY = 4;
+	public int xBoxLeftBumper = 5;
+	public int xBoxRightBumper = 6;
+	public int xBoxBack = 7;
+	public int xBoxStart = 8;
+
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
 
+	public int elevatorZone = 1;
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
+	
 	@Override
 	public void robotInit() {
 		oi = new OI();
@@ -63,16 +109,16 @@ public class Robot extends IterativeRobot {
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
 		
-	//copied from the Mecanum Drive example class	
-		//was in a separate class before, public Robot (){
+		// copied from the Mecanum Drive example class; was in a separate class before: public Robot (){
 		robotDrive = new MecanumDrive(kFrontLeftChannel, kRearLeftChannel, kFrontRightChannel, kRearRightChannel);
 	
 		kFrontLeftChannel.setInverted(true);
 		kRearLeftChannel.setInverted(true);
 		// invert the left side motors
-		//may need to change or remove to match the robot
+		// may need to change or remove to match the robot
 		
 		robotDrive.setExpiration(0.1);
+	
 	}
 
 	/**
@@ -89,6 +135,7 @@ public class Robot extends IterativeRobot {
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
+	
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
@@ -133,6 +180,16 @@ public class Robot extends IterativeRobot {
 		// this line or comment it out.
 		if (autonomousCommand != null)
 			autonomousCommand.cancel();
+		
+		// solenoid.set(DoubleSolenoid.Value.kOff);
+		// solenoid.set(DoubleSolenoid.Value.kForward);
+		solenoidTimer.reset();
+		solenoidTimer.start();
+
+		gyro.reset();
+		
+		climberSolenoid.set(DoubleSolenoid.Value.kForward);
+		
 	}
 
 	/**
@@ -141,13 +198,129 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		
 		//next line is from FRC Screen Steps Live 
 		//https://wpilib.screenstepslive.com/s/currentCS/m/java/l/599704-driving-a-robot-using-mecanum-drive
 		//use x and y to move forward or strafe (sliding), use z (twisty) axis to turn
 		//driver has to have the "sitting on the robot" mindset/mentality - "robot-oriented" driving
 //		robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getTwist(),0);
 		
-		robotDrive.driveCartesian(stick.getX(), stick.getY(), stick.getTwist(), 0.0);
+		boolean groundLimitPressed = !elevatorGroundLimit.get();
+		boolean switchLimitPressed = !elevatorSwitchLimit.get();
+		boolean scaleLimitPressed = !elevatorScaleLimit.get();
+		boolean maxLimitPressed = !elevatorMaxLimit.get();
+		
+		double xBoxLeftTrigger = xBox.getRawAxis(2);
+		double xBoxRightTrigger = xBox.getRawAxis(3);
+		double xBoxLeftJoystickY = xBox.getRawAxis(1);
+		double xBoxRightJoystickY = xBox.getRawAxis(5);
+		
+		double leftJoystickX = leftStick.getX();
+		double leftJoystickY = leftStick.getY();
+		double rightJoystickX = rightStick.getX();
+		double rightJoystickY = rightStick.getY();
+		 //gonna have to put boolean to make sure climber code doesn't run unless at right height
+				
+		if (xBox.getRawButton(xBoxStart)){
+			climber1.set(0.5);
+			climber2.set(0.5);
+			//we don't want to use the joystick for the climber; use the START button to climb once it gets to X climber height
+		} else {
+			climber1.set(0);
+			climber2.set(0);
+		}
+		
+		
+		/*if (Math.abs(xBoxRightJoystickY) > 0.1){
+				if (xBoxRightJoystickY < 0 && elevatorGroundLimit.get())
+				elevatorMotor.set(xBoxRightJoystickY);
+			else if (xBoxRightJoystickY > 0 && elevatorMaxLimit.get());
+				elevatorMotor.set(xBoxRightJoystickY);
+			//elevator manual climbing
+		}*/
+		
+		if (Math.abs(xBoxRightJoystickY) > 0.1) {
+			elevatorMotor.set(xBoxRightJoystickY);
+		} else {
+			elevatorMotor.set(0);
+		}
+		
+		//pressed  = false
+		//not pressed = true	
+		
+		//elevatorZone = checkZone(groundLimitPressed, switchLimitPressed, scaleLimitPressed, maxLimitPressed, elevatorZone, xBoxRightJoystickY);
+    
+		if(xBoxRightTrigger > 0){
+			intakeRight.set(0.8);
+			intakeLeft.set(0.8);
+		}
+		else if (xBoxLeftTrigger > 0){
+			intakeRight.set(-0.8);
+			intakeLeft.set(-0.8);
+		}
+		else {
+			intakeRight.set(0);
+			intakeLeft.set(0);
+		}
+		
+		
+		//double minX = Math.min(Math.abs(leftJoystickX), Math.abs(rightJoystickX));
+		if (Math.abs(rightJoystickX) > 0.2) {
+			kFrontLeftChannel.set(-rightJoystickX);
+			kRearRightChannel.set(-rightJoystickX);
+			kFrontRightChannel.set(rightJoystickX);
+			kRearLeftChannel.set(rightJoystickX);
+			//manual strafing
+		} else {
+			kFrontLeftChannel.set(leftJoystickY);
+			kRearLeftChannel.set(leftJoystickY);
+			kFrontRightChannel.set(rightJoystickY);
+			kRearRightChannel.set(rightJoystickY);
+			//manual tank drive
+		}
+		//robotDrive.driveCartesian(rightJoystickY, -rightJoystickX, -rightStick.getZ(), 0);
+		//robotDrive2.tankDrive(leftStick.getY(), rightStick.getY());
+		/*if(Math.abs(rightJoystickX) > 0.1 || Math.abs(leftJoystickX)> 0.1){
+			robotDrive.driveCartesian(rightStick.getX(), rightStick.getY(), leftStick.getX(), gyro.getAngle());
+		}else{
+			//robotDrive2.tankDrive(leftStick.getY(), rightStick.getY());
+		}*/
+		//makes pneumatics shoot out if right trigger is pressed and shoot back in when released
+		if(xBox.getRawButton(xBoxRightBumper)){
+			//button 6 is right bumper
+			intakeSolenoid.set(DoubleSolenoid.Value.kForward);
+		}
+		else if(xBox.getRawButton(xBoxLeftBumper)){
+			intakeSolenoid.set(DoubleSolenoid.Value.kReverse);
+		}
+		else{
+			intakeSolenoid.set(DoubleSolenoid.Value.kOff);
+		}
+			
+	/*	here: if the top limit switch has not been pressed yet
+			you can keep moving up
+		if it is moving downward && the bottom limit switch has not been pressed 
+		should we have a state variable? 
+		do all this logic in the elevator subsystem code. ;
+	*/
+		
+		if (xBox.getRawButton(xBoxBack)) {
+			//7 SHOULD BE THE BACK BUTTON
+			climberSolenoid.set(DoubleSolenoid.Value.kReverse);
+		}
+		else if (xBox.getRawButton(xBoxStart)){
+			//8 should be START button
+			climberSolenoid.set(DoubleSolenoid.Value.kForward);
+		}
+		
+		
+		
+		//we can change it to toggle with the button later.... talk to the drivers. 
+
+		
+		//right joystick for forwards/back and strafing
+		//left joystick controlls yaw (spinning)g
+		
 		
 		//pretty sure the gyro might be able to align the robot to the field, it will turn based on the field
 		//IF YOU ADD ANOTHER PARAMETER, THE GYRO ANGLE, IT BECOMES FIELD-ORIENTED
@@ -159,6 +332,13 @@ public class Robot extends IterativeRobot {
 //		robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getTwist(), gyro.getAngle());
 		
 		//we have to pick one or the other for the two lines^^
+		
+		//trying to operate pneumatics 2/2/18
+		
+	    // solenoid.set(DoubleSolenoid.Value.kReverse);
+	
+
+		
     }
 	
 	//lets see if i can push changes :)
@@ -169,4 +349,46 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void testPeriodic() {
 	}
+	
+	public int MAKINGTHISAREDXcheckZone(boolean groundSwitchPressed, boolean switchSwitchPressed, boolean scaleSwitchPressed, 
+			boolean maxSwitchPressed, int currentZone, double elevatorSpeed) {
+		int zone = 0;
+		if (groundSwitchPressed)
+			zone = 1;
+		else if (currentZone == 1 && !groundSwitchPressed && elevatorSpeed > 0)
+			zone = 2;
+		else if (currentZone == 3 && !switchSwitchPressed && elevatorSpeed < 0)
+			zone = 2;
+		else if (switchSwitchPressed)
+			zone = 3;
+		else if (currentZone == 3 && !switchSwitchPressed && elevatorSpeed > 0)
+			zone = 4;
+		else if (currentZone == 5 && !scaleSwitchPressed && elevatorSpeed < 0)
+			zone = 4;
+		else if (scaleSwitchPressed)
+			zone = 5;
+		else if (currentZone == 5 && !scaleSwitchPressed && elevatorSpeed > 0)
+			zone = 6;
+		else if (currentZone == 7 && !maxSwitchPressed && elevatorSpeed < 0)
+			zone = 6;
+		else if (maxSwitchPressed)
+			zone = 7;
+		return zone;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
